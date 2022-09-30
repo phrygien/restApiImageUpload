069139c3ef5e0c6e3fe2cd5978ad15defbdefe60
Mecene Phrygien <mecenephrygien@gmail.com>
2022-09-30 06:50:51 +0100

    mise en place endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', [ProductController::class, 'index']); 
+Route::get('products/{id}', [ProductController::class, 'show']); 
+Route::post('products', [ProductController::class, 'store']); 
+Route::put('products/{id}', [ProductController::class, 'update']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
